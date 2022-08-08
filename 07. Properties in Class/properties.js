64af183c63bc8be232819c5f6e82527eee412620
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-08 17:11:28 +0700

    Properties in Class

---
+++ 07. Properties in Class/properties.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(name, age) {
+    // we can add the properties in the constructor
+    this.name = name; // this is a property of the class
+    this.age = age; // this is a property of the class
+  }
+}
+const person1 = new Person("Daffa", 20);
+console.log(person1);
